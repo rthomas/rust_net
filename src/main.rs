7908c109d7bb261f9d56f4037044c813ecb66618
Ryan Thomas <ryan@ryant.org>
2017-03-15 20:04:44 +0000

    Move tap device creation into own module.
    Push out the tuntap / unsafe work into a module to use for all device
    interactions.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,7 @@
-use std::ffi::CString;
-use std::os::unix::io::RawFd;
+mod tuntap;
 
-extern crate libc;
 
 fn main() {
-    tun_init("tap0");
-}
-
-extern "C" {
-    fn tun_alloc(dev: *const libc::c_char) -> libc::c_int;
-}
-
-fn tun_init(name: &str) {
-    let fd: RawFd = unsafe {
-        tun_alloc(CString::new(name).unwrap().as_ptr())
-    };
-
-    println!("{:?}, {:?}", name, fd);
+    let tap = tuntap::TapDevice::new("tap0");
+    println!("{}", tap.unwrap());
 }
---
+++ src/tuntap.rs
@@ -0,0 +1,39 @@
+use std::fmt;
+use std::fmt::{Display, Formatter};
+use std::ffi::CString;
+use std::os::unix::io::RawFd;
+
+extern crate libc;
+
+extern "C" {
+    fn tun_alloc(dev: *const libc::c_char) -> libc::c_int;
+}
+
+pub struct TapDevice {
+    dev_name: String,
+    tap_fd: RawFd,
+}
+
+impl Display for TapDevice {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "TapDevice({})", self.dev_name)
+    }
+}
+
+impl TapDevice {
+    pub fn new(dev_name: &str) -> Result<TapDevice, String> {
+        let fd: RawFd = unsafe {
+            tun_alloc(CString::new(dev_name).unwrap().as_ptr())
+        };
+
+        if fd > 0 {
+            println!("Allocated {:?} as fd: {:?}", dev_name, fd);
+            Ok(TapDevice {dev_name: String::from(dev_name), tap_fd: fd})
+        }
+        else {
+            Err(format!("Unable to create device: Error {}", fd))
+        }
+    }
+
+    // TODO - impl read and write from the device.
+}
