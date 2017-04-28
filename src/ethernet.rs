0539c8c197626da9ff950cdf68ce24b1d2a679b1
Ryan Thomas <ryan@ryant.org>
2017-04-28 18:03:48 +0100

    Add in the frame type parsing.
    Added in the ethertype lookup for a small subset and fixed up the
    EthernetFrame to use the enum instead of a raw array.

--- src/ethernet.rs
+++ src/ethernet.rs
@@ -1,17 +1,27 @@
+use std::collections::HashMap;
 use std::io::Read;
 
 use tuntap;
 
 // 1522 is the max length of an L2 ethernet frame, up to the end of the CRC.
 const ETH_MAX_FRAME_SIZE: usize = 1522;
+const ETH_MAX_PAYLOAD: u16 = 1500;
 const ETH_ALEN: usize = 6;
+
+#[derive(Debug)]
+enum EtherType {
+    Payload,
+    IPv4,
+    Arp,
+    IPv6,
+}
 
 #[derive(Debug)]
 pub struct EthernetFrame {
     dest_mac: [u8; ETH_ALEN],
     source_mac: [u8; ETH_ALEN],
     tag: Option<[u8; 4]>,
-    ethertype: [u8; 2],
+    ethertype: EtherType,
     payload: Box<Vec<u8>>,
     crc: [u8; 4],
 }
@@ -19,6 +29,27 @@ pub struct EthernetFrame {
 pub struct Ethernet {
     dev: tuntap::TapDevice,
     buf: Vec<u8>,
+}
+
+/// Returns either the known ethertype or None.
+fn ethertype_lookup(key: [u8; 2]) -> Option<EtherType> {
+    let val  = (key[0] as u16) << 8 | (key[1] as u16);
+
+    if val <= ETH_MAX_PAYLOAD {
+        Some(EtherType::Payload)
+    }
+    else if val == 0x0800 {
+        Some(EtherType::IPv4)
+    }
+    else if val == 0x0806 {
+        Some(EtherType::Arp)
+    }
+    else if val == 0x86DD {
+        Some(EtherType::IPv6)
+    }
+    else {
+        None
+    }
 }
 
 impl Ethernet {
@@ -42,7 +73,11 @@ impl Ethernet {
             source_mac[i] = self.buf[i + ETH_ALEN];
         }
 
-        let ethertype: [u8; 2] = [self.buf[12], self.buf[13]];
+        let ethertype = match ethertype_lookup([self.buf[12], self.buf[13]]) {
+            Some(e) => e,
+            None => return Err(format!("Unknown EtherType: {:?}", [self.buf[12], self.buf[13]])),
+        };
+        
         let crc: [u8; 4] =
             [self.buf[len - 4], self.buf[len - 3], self.buf[len - 2], self.buf[len - 1]];
 
@@ -58,5 +93,18 @@ impl Ethernet {
             crc: crc,
         };
         Ok(frame)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_ethertype_lookup() {
+        match ethertype_lookup([0x08, 0x06]).unwrap() {
+            EtherType::Arp => (),
+            _ => panic!()
+        }
     }
 }
